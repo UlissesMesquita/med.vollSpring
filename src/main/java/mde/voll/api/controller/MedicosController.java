3d00b8a48490ae4229f45068b3b9d873c2ceb918
Ulisses Mesquita <desenvolvimento@xpon.com.br>
2023-11-03 20:44:46 -0300

    Adicionado interface e persistencia

--- src/main/java/mde/voll/api/controller/MedicosController.java
+++ src/main/java/mde/voll/api/controller/MedicosController.java
@@ -1,15 +1,22 @@
 package mde.voll.api.controller;
 
 import mde.voll.api.medico.DadosCadastroMedico;
+import mde.voll.api.medico.Medico;
+import mde.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicosController {
 
+    @Autowired
+    private MedicoRepository repository;
+
+
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico dados){
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 
 
